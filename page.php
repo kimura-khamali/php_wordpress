526f960fe81123067725ac5789945ff63c77cc7c
kimura-khamali <kimurakhamali@gmail.com>
2025-07-22 14:11:13 +0300

    added files

--- page.php
+++ page.php
@@ -1,15 +1,14 @@
-This is a page
-
 <?php 
 get_header();
+
 while(have_posts()){
     the_post();?>
-    <h2><?php the_title(); ?></h2>
-    <?php the_content(); ?>
+    <div class="container">
+        <h2><?php the_title(); ?></h2>
+        <?php the_content(); ?>
+    </div>
 <?php
 }
+
 get_footer();
 ?>
-
-
-
